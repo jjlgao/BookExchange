67117bd5191dcd42eaaa16bd72ea3395ed9d7f68
Maruf Rahman <marufrahman@berkeley.edu>
2016-11-16 17:46:41 -0800

    creates new user

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,5 +3,12 @@ class RegistrationController < ApplicationController
   end
 
   def new_seller
+  	@seller = Seller.new
+  	@seller.name = params[:name]
+
+  	@seller.save!
+  	
+  	redirect_to root_path
   end
+
 end
