7d44d843f4bae02aff45ce90c8e67c9f37408a6f
Maruf Rahman <marufrahman@berkeley.edu>
2016-11-16 17:25:41 -0800

    added registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'registration/register', to: 'registration#register'
+  post 'registration/create', to: 'registration#new_seller'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,6 +9,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   
   root 'home#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
