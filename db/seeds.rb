dd46d070ba55a51eb5b201444927cc63d0060c6f
Maruf Rahman <marufrahman@berkeley.edu>
2016-11-24 01:45:23 -0800

    styled the login and the registration page and added home button

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Book.create(name: 'Biology', price: 150.00, image: "https://www.mheonline.com/assets/images/cover/large/0078945860.jpg", seller_id: 5)
-Book.create(name: 'Calculus', price: 249.99, image: "https://images-na.ssl-images-amazon.com/images/I/51-G-148jJL._SX258_BO1,204,203,200_.jpg", seller_id: 6)
+# Book.create(name: 'Biology', price: 150.00, image: "https://www.mheonline.com/assets/images/cover/large/0078945860.jpg", seller_id: 5)
+# Book.create(name: 'Calculus', price: 249.99, image: "https://images-na.ssl-images-amazon.com/images/I/51-G-148jJL._SX258_BO1,204,203,200_.jpg", seller_id: 6)
 
-Seller.create(name: "Barack Obama", email: "obama@us.com")
-Seller.create(name: "Joe Biden", email: "biden@us.com")
+# Book.create(name: 'Chemistry', price: 50.00, image: "https://images-na.ssl-images-amazon.com/images/I/51GVJE1DjIL.jpg", seller_id: 5)
+
+# Seller.create(name: "Barack Obama", email: "obama@us.com")
+# Seller.create(name: "Joe Biden", email: "biden@us.com")
